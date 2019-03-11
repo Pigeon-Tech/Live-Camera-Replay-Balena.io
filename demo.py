37154b006e33e2e7e784d122d8137e2ca4ac2a33
Pigeon-Tech <44372309+Pigeon-Tech@users.noreply.github.com>
2019-03-11 15:29:44 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -7,7 +7,4 @@ with picamera.PiCamera() as camera:
     camera.resolution = (320, 240)
     # Camera warm-up time
     time.sleep(2)
-    camera.capture('/data/image.jpg')
-
-print 'Picture taken'
-time.sleep(10)
+    camera.start_recording('/data/video.h264')
